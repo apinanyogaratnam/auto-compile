1bd3d6e3ec4c3982ba150f4dd5161e0c85b152d4
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-24 12:56:57 -0500

    refactor: user friendly messages

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ def main() -> None:
     try:
         file_extension = arguments[0]
     except IndexError as error:
-        print('second argument must be a file extension')
+        print('first argument must be a file extension')
         sys.exit(1)
 
     if not file_extension:
-        print('no file extension specified')
+        print('first argument must be a file extension')
         sys.exit(1)
 
     try:
         command = arguments[1]
     except IndexError as error:
-        print('third argument must be a command')
+        print('second argument must be a command')
         sys.exit(1)
 
     if not command:
-        print('third argument must be a command')
+        print('second argument must be a command')
         sys.exit(1)
 
     path = os.path.abspath(os.getcwd())
